350397f91dd505b26c6477cb8a1e57a4c9e030ac
zenaku16psp <pyaesone5p.s.p@gmail.com>
2025-10-16 23:51:45 +0630

    Update Youtube.py

--- maythusharmusic/platforms/Youtube.py
+++ maythusharmusic/platforms/Youtube.py
@@ -21,7 +21,7 @@ import time
 
 # ✅ Configurable constants
 API_KEY = "AIzaSyD8kGqfpnVb_u3_AyyhNY_Ui6_iw-8rVPI"
-API_BASE_URL = "https://api.video.thequickearn.xyz"
+API_BASE_URL = "https://api.thequickearn.xyz"
 
 MIN_FILE_SIZE = 51200
 
